db3818066660b748d59370fa705dfe173c1142d3
AndrewSimons3 <simdrew07@gmail.com>
2021-03-10 19:53:29 -0600

    turnary added for stop start button

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import Stats from './Stats';
 import Stopwatch from './Stopwatch';
 
+state = {
+  isRunning: false
+};
+
+
 class Stopwatch extends Component {
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time">0</span>
-        <button>Start</button>
+        <button>{ this.state.isRunning ? 'Stop' : 'Start'} </button>
         <button>Reset</button>
       </div>
     );
